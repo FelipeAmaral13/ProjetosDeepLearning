232955b771cf717a0f32e6544ae884de6b19e093
FelipeAmaral13 <felipengmec@gmail.com>
2020-09-16 08:55:50 -0300

    Estudos do algoritmo

--- Yolo/yolo_teste2.py
+++ Yolo/yolo_teste2.py
@@ -5,34 +5,41 @@ import time
 #Load YOLO
 #net = cv2.dnn.readNet("yolov3.weights","yolov3.cfg")
 net = cv2.dnn.readNet("yolov3-tiny.weights","yolov3-tiny.cfg") 
-classes = []
 
+# Classes 
+classes = []
 with open("coco.names","r") as f:
     classes = [line.strip() for line in f.readlines()]
 
 #print(classes)
 
-layer_names = net.getLayerNames()
-outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-colors= np.random.uniform(0, 255, size=(len(classes), 3))
+layer_names = net.getLayerNames() # estrutura da rede pre-treinada
+outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()] # Camadas da rede nao conectadas
+
+colors= np.random.uniform(0, 255, size=(len(classes), 3)) # Cores para o boundingBoxes 
 
 
 #loading image
-cap=cv2.VideoCapture("1.mp4") 
+cap=cv2.VideoCapture(0) 
 
 while True:
     ret,frame= cap.read() 
     
-    height,width,channels = frame.shape
-    #detecting objects
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (256, 256), (0, 0, 0), True, crop=False)  
+    height, width, channels = frame.shape
 
-        
-    net.setInput(blob)
-    outs = net.forward(outputlayers)
+    #Detectando objetos 
+    # Funcao blob - basicamente extrai as caracteristicas dos objs
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (320, 320), (0, 0, 0), True, crop=False)         
+    net.setInput(blob) # Passar as caracteristicas obtidas no blob na rede
+    outs = net.forward(outputlayers) # Extrair todas as informacoes do objeto captado
     #print(outs[1])
 
+    """
+    Com todas as informacoes obtidas e preciso fazer as classificacoes, primeiramente encontra-se um score
+    para cada informacao do 'outs'. Depois pega-se a classe com o maior score encontrada e compara-se com uma 
+    confianca.
+    """
 
     class_ids=[]
     confidences=[]
@@ -42,7 +49,7 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.1:
+            if confidence > 0.5:
                 #Objeto detectato
                 center_x= int(detection[0]*width)
                 center_y= int(detection[1]*height)
@@ -55,11 +62,12 @@ while True:
 
                 boxes.append([x,y,w,h]) # Pegando as areas do retang
                 confidences.append(float(confidence)) # Porcentagem do obj em relacao a confianca
-                class_ids.append(class_id) #calsse do obj
+                class_ids.append(class_id) #classe do obj
 
+    # Eliminar caixas duplas do mesmo objeto
     indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.5,0.4)
 
-
+    # Fazer os boundigboxes
     for i in range(len(boxes)):
         if i in indexes:
             x,y,w,h = boxes[i]
@@ -80,5 +88,4 @@ while True:
 cap.release()    
 cv2.destroyAllWindows()
 
-# credito: https://medium.com/analytics-vidhya/real-time-object-detection-using-yolov3-with-opencv-and-python-64c985e14786
-#           https://stackabuse.com/object-detection-with-imageai-in-python/ 
+ 
